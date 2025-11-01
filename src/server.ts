a138fcb2791c69610041ed83bf8830a68b69cc5f
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-11-01 14:08:00 -0400

    Add CORS support to Express server
    Installed 'cors' and '@types/cors' packages and configured CORS middleware in the Express server to allow requests from specified origins. This improves security and enables cross-origin requests from the frontend.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import WaitListRoutes from "./routes/v1/waitlist.routes";
-import * as z from "zod";
-import prisma from "./utils/db";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+
+const allowedOrigins = [process.env.FRONTEND_URL!, "http://localhost:8000"];
+
+const corsOption = {
+  origin: function (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // If you need to send cookies
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
 app.use(express.json());
+app.use(cors(corsOption));
 
 app.use("/api/v1/waitlist", WaitListRoutes);
 
