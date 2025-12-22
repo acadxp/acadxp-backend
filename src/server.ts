a4715bba87af4f824a9879dcc778a29bf16cb0a9
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-12-22 14:29:25 -0500

    Enable CORS preflight for all routes
    Add CORS preflight handling for all routes.

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,7 @@ const corsOption = {
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 app.use(cors(corsOption));
+app.options(/.*/, cors(corsOption));
 // app.options("*", cors(corsOption)); // Handle preflight for all routes
 
 app.use(express.json());
